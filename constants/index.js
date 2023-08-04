9cd7484c4592c94675957fae9a9d6c411da61aa7
armita63 <140148835+armita63@users.noreply.github.com>
2023-08-04 16:37:20 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xfd6c4941d4477ab0d296c61791dda9edcd6e22f5";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
